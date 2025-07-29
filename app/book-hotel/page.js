e8ce4539d8dcd53c15b30e7ea455d17989c2c169
eschlessinger <emmie.schlessinger@gmail.com>
2025-07-29 17:04:27 -0400

    Update page.js

--- app/book-hotel/page.js
+++ app/book-hotel/page.js
@@ -26,7 +26,7 @@ export default function BookHotelPage() {
     { 
       label: "LINEUP", 
       sublabel: "",
-      href: "/launch-page", 
+      href: "/lineup", 
       disabled: false,
       onClick: null
     },
@@ -76,7 +76,7 @@ export default function BookHotelPage() {
         {/* Header */}
         <div className="p-4 md:p-6 lg:p-8">
           <div className="flex justify-center">
-            <Link href="/launch-page" className="w-full max-w-sm md:max-w-md">
+            <Link href="/lineup" className="w-full max-w-sm md:max-w-md">
               <Image
                 src="/bremmiepalooza-logo-for-cta.png"
                 alt="Bremmiepalooza 2026"

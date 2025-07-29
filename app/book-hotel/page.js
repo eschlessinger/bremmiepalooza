2f5a905030e79a881b159ae6bbe89d34319df28a
eschlessinger <emmie.schlessinger@gmail.com>
2025-07-29 15:31:47 -0400

    Update page.js

--- app/book-hotel/page.js
+++ app/book-hotel/page.js
@@ -105,7 +105,7 @@ export default function BookHotelPage() {
         >
           <div 
             className="bg-gradient-to-r from-yellow-300 via-pink-300 to-blue-300" 
-            style={{ height: isMobile ? '140px' : '180px' }}
+            style={{ height: isMobile ? '120px' : '160px' }}
           >
             <div className="flex justify-center items-center gap-4 md:gap-8 px-2 h-full relative">
               {navButtons.map((button, index) => {
@@ -344,7 +344,7 @@ export default function BookHotelPage() {
         </div>
 
         {/* Content with margin for absolute positioned banner */}
-        <div style={{ marginTop: isMobile ? '140px' : '180px' }}>
+        <div style={{ marginTop: isMobile ? '120px' : '160px' }}>
           
           {/* Main Content */}
           <div className="px-4 pb-12">

b8c0434b254f2898d2e8c689067806a7ed1fa794
eschlessinger <emmie.schlessinger@gmail.com>
2025-08-08 15:47:04 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function LineupPage() {
     { 
       label: "LINEUP", 
       sublabel: "",
-      href: "/lineup", 
+      href: "/", 
       disabled: false,
       onClick: null
     },
@@ -105,7 +105,7 @@ export default function LineupPage() {
         {/* Header */}
         <div className="p-4 md:p-6 lg:p-8">
           <div className="flex justify-center">
-            <Link href="/lineup" className="w-full max-w-sm md:max-w-md">
+            <Link href="/" className="w-full max-w-sm md:max-w-md">
               <Image
                 src="/bremmiepalooza-logo-for-cta.png"
                 alt="Bremmiepalooza 2026"

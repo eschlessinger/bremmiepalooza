affbcd29b8ba911e620fe2d6d4d4c6ea0f36ae46
eschlessinger <emmie.schlessinger@gmail.com>
2025-08-11 17:23:59 -0400

    Update BremmieDoodle.tsx

--- components/BremmieDoodle.tsx
+++ components/BremmieDoodle.tsx
@@ -1,29 +1,22 @@
-interface BremmieDoodleProps {
-  className?: string
-  style?: React.CSSProperties
-}
+// components/Bremmie.tsx
+"use client";
 
-export default function BremmieDoodle({ className = "", style = {} }: BremmieDoodleProps) {
-  const defaultStyle = {
-    right: "var(--bremmie-right, 6%)",
-    bottom: "var(--bremmie-bottom, 60px)",
-    width: "var(--bremmie-size, 20vmin)",
-    height: "var(--bremmie-size, 20vmin)",
-    ...style
-  }
-
+export default function Bremmie() {
   return (
     <div
-      className={`fixed pointer-events-none z-20 ${className}`}
-      style={defaultStyle}
+      className="fixed pointer-events-none z-20"
+      style={{
+        right: "var(--bremmie-right)",
+        bottom: "var(--bremmie-bottom)",
+        width: "var(--bremmie-size)",
+        height: "var(--bremmie-size)",
+      }}
     >
-      <div className="relative w-full h-full">
-        <img 
-          src="/doodles/bremmie.png" 
-          alt="Bremmie on the beach" 
-          className="w-full h-full object-contain"
-        />
-      </div>
+      <img
+        src="/doodles/bremmie.png"
+        alt="Bremmie on the beach"
+        className="w-full h-full object-contain"
+      />
     </div>
-  )
+  );
 }
